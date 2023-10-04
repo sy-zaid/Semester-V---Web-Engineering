0b5041286a341b92103f96be2c98eb0dca8beeac
SyedZaid <syedmzaid.99@gmail.com>
2023-10-05 01:35:57 +0500

    Initiating task 3A

---
+++ script.js
@@ -0,0 +1,8 @@
+// Task - 3 | JAVASCRIPT
+function findInputs(inp1,inp2){
+    // console.log(inp1)
+    let a = document.getElementById(inp1).value
+    let b = document.getElementById(inp2).value
+    console.log(a,b)
+
+}
