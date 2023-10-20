1fcf1a154ee806d74b7c7d73456c00c9b547928f
SyedZaid <syedmzaid.99@gmail.com>
2023-10-20 19:25:46 +0500

    made css files and added searching on table

--- Lab - 3/script.js
+++ Lab - 3/script.js
@@ -1,100 +1,228 @@
-// // $(document).ready(function(){
-// //   $(table).click(function(){
-// //     $(this).hide()
-// //   })
-// //   console.log('Hi, lab 3')
-// // })
+function addNewStudent() {
+  const body = document.querySelector("tbody");
+  const row = document.createElement("tr");
+  const tdStudentID = document.createElement("td");
+  const tdStudentName = document.createElement("td");
+  const tdBatch = document.createElement("td");
+  const tdAction = document.createElement("td");
+  const studentId = Math.round(Math.random() * 100) + 4;
+  const studentName = "New Student " + studentId;
 
-// // ------------------------- Lab Task 5 -------------------------
-// $("#studentNames").on({
-//   mouseenter: function(){
-//   $(this).css("background-color", "lightgray");
-//   },
-//   mouseleave: function(){
-//   $(this).css("background-color", "lightblue");
-//   },
-//   click: function(){
-//   $(this).css("background-color", "yellow");
-//   }
-//   });
+  const batch = Math.round(Math.random() * 5) + 2018;
+  row.id = "row" + studentId;
+  tdStudentID.innerHTML = "Student " + studentId;
+  tdStudentName.innerHTML = studentName;
+  tdBatch.innerHTML = batch;
+  tdAction.innerHTML =
+    '<button id="table-button" onclick="updateStudent(\'' +
+    row.id +
+    "')\">Update</button>";
+  row.appendChild(tdStudentID);
+  row.appendChild(tdStudentName);
+  row.appendChild(tdBatch);
+  row.appendChild(tdAction);
+  body.appendChild(row);
+}
+function deleteStudent() {
+  const studentId = prompt("Please enter Student # to delete:");
+  const row = document.getElementById("row" + studentId);
+  if (row) {
+    const body = document.querySelector("tbody");
+    body.removeChild(row);
+  } else {
+    alert("Student not found or invalid input.");
+  }
+}
+function updateStudent(rowId) {
+  const row = document.getElementById(rowId);
+  if (row) {
+    // Prompt the user to enter updated student data
+    const updatedName = prompt("Enter updated student name:");
+    const updatedBatch = prompt("Enter updated batch:");
+    // Update the table cell values
+    row.querySelector("td:nth-child(2)").textContent = updatedName;
+    row.querySelector("td:nth-child(3)").textContent = updatedBatch;
+  } else {
+    alert("Student not found or invalid input.");
+  }
+}
 
-// // // ------------------------- Lab Task  6 -------------------------//
-// $("#studentNames").click(function () {
-//   $("studentNames").hide();
-// });
+// ------------------------- LAB TASK ------------------------- //
+function updateBatch() {
+  let batchno = window.prompt(
+    "Enter the student number and batch number in comma seperated values"
+  );
+  batchno = batchno.split(",");
+  let stdno = batchno[0];
+  let batch = batchno[1];
+  // console.log(batch,stdno)
+  let elem_row = document.getElementById("row" + stdno);
+  elem_row.querySelector("td:nth-child(3)").textContent = batch;
+}
 
-// $("#studentNames").click(function () {
-//   $("studentNames").show();
-// });
+// ------------------------- LAB # 02 | STUDENT TASKS ------------------------- //
 
-// // ------------------------- Lab Task 10 -------------------------//
-// $(document).ready(function () {
-//   $("#myInput").on("keypress", function (e) {
-//     if (e.which == 13) {
-//       var value = $(this).val().toUpperCase();
-//       $.ajax({
-//         url: "https://open.er-api.com/v6/latest/" + value,
-//         dataType: "json",
-//         type: "GET",
-//         success: function (data, status, xhr) {
-//           var oRates = data.rates;
-//           var html = "";
-//           for (var o in oRates) {
-//             if (oRates.hasOwnProperty(o)) {
-//               html += `<tr><td>${o}</td><td>${oRates[o]}</td></tr>`;
-//             }
-//           }
-//           $("#TBody").html(html);
-//         },
-//         error: function (jqXhr, textStatus, errorMessage) {
-//           alert(errorMessage);
-//         },
-//       });
-//     }
-//   });
-// });
+function addNewCourse() {
+  const body = document.querySelector("tbody");
+  const row = document.createElement("tr");
+  const tdCourseID = document.createElement("td");
+  const tdCourseName = document.createElement("td");
+  const tdChrs = document.createElement("td");
+  const tdYear = document.createElement("td");
+  const tdAction = document.createElement("td");
 
-// // -----EVENT Listeners-----//
+  let newcourse = prompt(
+    "Add details in comma seperated values: (Code, Name, Credit Hours, Year)"
+  );
+  newcourse = newcourse.split(",");
 
-// $(document).ready(function () {
-//   $("#abc").on({
-//     click: function () {
-//       $(this).css("background-color", "lightgray");
-//       // $('#abc').hide()
-//     },
-//     dblclick: function () {
-//       $(this).css("background-color", "lightblue");
-//     },
-//     hover: function () {
-//       $(this).css("background-color", "yellow");
-//     },
-//   });
-// });
-// // Wait for the document to be ready
-// $(document).ready(function () {
-//   // Attach a click event handler to the button element
-//   $("button").click(function () {
-//     // Use the animate() function to change the left position of all div elements
-//     $("div").animate(
-//       {
-//         left: "250px", // Set the new left position
-//       },
-//       "slow"
-//     ); // Specify the animation duration ('slow' for a slower animation)
-//   });
-// });
+  const CourseId = newcourse[0];
+  const CourseName = newcourse[1];
+  const Chrs = newcourse[2];
+  const year = newcourse[3];
 
-// $(document).ready(function () {
-//   $("button").click(function () {
-//     $(".test").hide();
-//   });
-// });
+  row.id = CourseId;
 
-// --------------- LAB TASK ---------------
+  tdCourseID.innerHTML = CourseId;
+  tdCourseName.innerHTML = CourseName;
+  tdYear.innerHTML = year;
+  tdChrs.innerHTML = Chrs;
+  tdAction.innerHTML =
+    '<button id="table-button" onclick="updateCourse(\'' +
+    row.id +
+    "')\">Update</button>";
+  row.appendChild(tdCourseID);
+  row.appendChild(tdCourseName);
+  row.appendChild(tdChrs);
+  row.appendChild(tdYear);
+  row.appendChild(tdAction);
+  body.appendChild(row);
+}
+
+function updateCourse(rowId) {
+  const row = document.getElementById(rowId);
+  if (row) {
+    const updatedcode = prompt("Enter updated course code:");
+    row.querySelector("td:nth-child(1)").textContent = updatedcode;
+
+    const updatedName = prompt("Enter updated course Name:");
+    row.querySelector("td:nth-child(2)").textContent = updatedName;
+
+    const updatedChrs = prompt("Enter updated course Chrs:");
+    row.querySelector("td:nth-child(3)").textContent = updatedChrs;
+
+    const updatedYear = prompt("Enter year:");
+    row.querySelector("td:nth-child(4)").textContent = updatedYear;
+  } else {
+    alert("Course not found or invalid input.");
+  }
+}
+
+function removeCourse() {
+  let courseId = prompt("Please enter Course code to delete that course:");
+
+  const row = document.getElementById(courseId);
+  if (row) {
+    const body = document.querySelector("tbody");
+    body.removeChild(row);
+  } else {
+    alert("Course not found or invalid input.");
+  }
+}
+
+function searchCourse() {
+  const searchquery = prompt("Enter course code to search");
+  const row = document.querySelector(`#${searchquery}`);
+  row.style.backgroundColor = "yellow";
+  setTimeout(function () {
+    row.style.backgroundColor = "white";
+    row.style.transition = "all ease 2s";
+  }, 1500);
+}
+
+// ------------------------- Lab Task 5 -------------------------
+$("#row1, #row2, #row3").on({
+  mouseenter: function () {
+    $(this).css("background-color", "lightgray");
+  },
+  mouseleave: function () {
+    $(this).css("background-color", "lightblue");
+  },
+  click: function () {
+    $(this).css("background-color", "yellow");
+  },
+});
+
+// // ------------------------- Lab Task  6 -------------------------//
+$("#studentNames").click(function () {
+  $("studentNames").hide();
+});
+
+$("#studentNames").click(function () {
+  $("studentNames").show();
+});
+
+// ------------------------- Lab Task 10 -------------------------//
+$(document).ready(function () {
+  $("#myInput").on("keypress", function (e) {
+    if (e.which == 13) {
+      var value = $(this).val().toUpperCase();
+      $.ajax({
+        url: "https://open.er-api.com/v6/latest/" + value,
+        dataType: "json",
+        type: "GET",
+        success: function (data, status, xhr) {
+          var oRates = data.rates;
+          var html = "";
+          for (var o in oRates) {
+            if (oRates.hasOwnProperty(o)) {
+              html += `<tr><td>${o}</td><td>${oRates[o]}</td></tr>`;
+            }
+          }
+          $("#TBody").html(html);
+        },
+        error: function (jqXhr, textStatus, errorMessage) {
+          alert(errorMessage);
+        },
+      });
+    }
+  });
+});
+
+// -----EVENT Listeners-----//
+
+$(document).ready(function () {
+  $("#abc").on({
+    click: function () {
+      $(this).css("background-color", "lightgray");
+      // $('#abc').hide()
+    },
+    dblclick: function () {
+      $(this).css("background-color", "lightblue");
+    },
+    hover: function () {
+      $(this).css("background-color", "yellow");
+    },
+  });
+});
+
+$(document).ready(function () {
+  $("button").click(function () {
+    $("div").animate(
+      {
+        left: "250px",
+      },
+      "slow"
+    );
+  });
+});
+
+
+// --------------- LAB TASK | Searching through a table---------------//
 $(document).ready(function () {
   $("#search").on("input", function () {
     var value = $(this).val().toLowerCase();
-    $("#table tr").filter(function () {
+    $("#table-data tr").filter(function () {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
